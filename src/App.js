41e7ebc2d8b83a73e94a38bb31adccc434a25c60
Shritej Mayekar <shritejmayekar@gmail.com>
2022-08-03 12:03:10 +0530

    [shritej] Modified: dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import AppRouter from './router/router';
 
-
 function App() {
   return (
     <div className="App">
--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,9 @@
-module.exports = {
-    apiUrl:process.env.REACT_APP_API,
-    googleTrackingId:process.env.GOOGLE_ANALYTICS_TRACKING_ID
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+const env = runtimeEnv();
+
+const myEnvironment = {
+    apiUrl:env.REACT_APP_API,
+    googleTrackingId:env.GOOGLE_ANALYTICS_TRACKING_ID
 }
+
+export default myEnvironment;
