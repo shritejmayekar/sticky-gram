f79de82db81b9ba667dc2c504b0d91b5d04cf1dc
Shritej Mayekar <shritejmayekar@gmail.com>
2022-08-09 13:54:38 +0530

    [shritej] minor changes tracking

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   console.log('location', location);
   React.useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
-  }, [location]);
+  }, [location.pathname]);
 
   return (
     <div className="App">
