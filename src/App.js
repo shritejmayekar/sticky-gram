ea97577fa5afd2de25becc07db4dafe7fe049c00
Shritej Mayekar <shritejmayekar@gmail.com>
2022-08-10 13:42:18 +0530

    checking ga

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import AppRouter from './router/router';
 import ReactGA from 'react-ga';
 import {useLocation} from 'react-router-dom';
 import React from "react";
-ReactGA.initialize({ trackingId: process.env.googleTrackingId });
+import useGaTracker from './router/useTracker';
+// ReactGA.initialize({ trackingId: process.env.googleTrackingId });
 
 function App() {
-  const location = useLocation();
-  console.log('hash', location.hash);
-  console.log('pathname', location.pathname);
-  console.log('search', location.search);
-  console.log('location', location);
-  React.useEffect(() => {
-    console.log('in',window.location.pathname)
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, [location]);
+  // let location = useLocation();
+  // console.log('hash', location.hash);
+  // console.log('pathname', location.pathname);
+  // console.log('search', location.search);
+  // console.log('location', location);
+  // React.useEffect(() => {
+  //   console.log('in',window.location.pathname)
+  //   ReactGA.pageview(location.pathname);
+   
+  //   // ReactGA.set({ page: location.pathname });
+  //   // ReactGA.pageview(location.pathname)
+  //   // ReactGA.ga('send', 'pageview', location.pathname);
 
+  // }, [location]);
+  useGaTracker();
   return (
     <div className="App">
      <AppRouter />
---
+++ src/router/useTracker.js
@@ -0,0 +1,23 @@
+import React,{useEffect,useState} from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from 'react-ga';
+
+const useGaTracker = () => {
+    const location = useLocation();
+    const [initialized, setInitialized] = useState(false);
+
+    useEffect(() => {
+        if (!window.location.href.includes("localhost")) {
+            ReactGA.initialize(process.env.googleTrackingId);
+        }
+        setInitialized(true);
+    }, []);
+
+    useEffect(() => {
+        if (initialized) {
+        ReactGA.pageview(location.pathname + location.search);
+        }
+    }, [initialized, location]);
+};
+
+export default useGaTracker;
